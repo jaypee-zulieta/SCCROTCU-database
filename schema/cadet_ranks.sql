91a818dc01c7e445d27e160a2318bf4ac555d84c
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-01 11:53:32 +0800

    feat: Add schema file for cadet_ranks
    Add cadet_ranks table which is connected to the
    ranks, classes and cadet_officers table so that
    the promotion of ranks can be tracked over time.

---
+++ schema/cadet_ranks.sql
@@ -0,0 +1,12 @@
+-- Run ranks.sql, cadet_officers.sql and classes.sql 
+-- before running this script
+CREATE TABLE IF NOT EXISTS cadet_ranks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  cadet_id INT NOT NULL,
+  rank_id INT NOT NULL,
+  class_id INT NOT NULL,
+  given_on DATE,
+  FOREIGN KEY(cadet_id) REFERENCES cadet_officers(enrolled_cadet_id),
+  FOREIGN KEY(rank_id) REFERENCES ranks(id),
+  FOREIGN KEY(class_id) REFERENCES classes(id)
+);
---
+++ schema/ranks.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS ranks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  abbreviated_name VARCHAR(50) UNIQUE NOT NULL,
+  descriptive_name VARCHAR(255) NOT NULL
+);
