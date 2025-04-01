8c7a157808db0c852a82b285655ad4687ef14a4c
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-01 09:56:46 +0800

    feat: Add schema file for attendance_records
    Add attendance_records table and so that the history
    of attendances from all the cadets can be tracked

---
+++ schema/attendance_records.sql
@@ -0,0 +1,12 @@
+-- Run cadets.sql and formations.sql before running this script.
+CREATE TABLE IF NOT EXISTS attendance_records (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  formation_id INT NOT NULL,
+  enrolled_cadets_id INT NOT NULL,
+  attendance_status VARCHAR(50) NOT NULL,
+  remarks TEXT,
+  FOREIGN KEY(formation_id) REFERENCES formations(id),
+  FOREIGN KEY(enrolled_cadets_id) REFERENCES cadets(id),
+  CONSTRAINT attendance_status_check
+  CHECK (attendance_status IN ("Present", "Absent", "Excused"))
+);
