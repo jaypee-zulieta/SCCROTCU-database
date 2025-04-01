f7b67f9ce567a715165d33e0ab2e133a2dd540f4
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-01 09:48:09 +0800

    feat: Add schema file for formations
    Add formations table which is connected to the
    semesters table via foreign key so that we can
    track the formations a cadet attended and from
    what semester the formation was held on.

---
+++ schema/formations.sql
@@ -0,0 +1,10 @@
+-- Run semester.sql before running this script
+CREATE TABLE IF NOT EXISTS formations (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  semester_id INT NOT NULL,
+  descriptive_name VARCHAR(255) NOT NULL,
+  conducted_on DATE NOT NULL,
+  started_on TIME,
+  ended_on TIME,
+  FOREIGN KEY (semester_id) REFERENCES semesters(id)
+);
