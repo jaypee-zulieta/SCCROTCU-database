ae4b69e43f16ea136faa8dae51412e2cbc87397d
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-01 08:35:21 +0800

    feat: Add schema file for cadets
    Add cadets table to store the personal data of
    cadets in the system.

---
+++ schema/cadets.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS cadets (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  edp_number VARCHAR(50) UNIQUE,
+  last_name VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  middle_name VARCHAR(255),
+  sex VARCHAR(50),
+  date_of_birth DATE,
+  CONSTRAINT sex_constraint
+  CHECK (sex IN ("M", "F"))
+);
