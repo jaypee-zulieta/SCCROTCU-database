324ccc6aaa811508229d33f948d6e17452374ead
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-03 04:22:05 +0800

    feat: Add schema file for platoon_leaders table
    Add platoon_leader table, connected to the
    cadet_officers and the platoons table via foreign
    key, so that we can find who the designated
    platoon leader is for a certain platoon and when
    they were designated on that platoon.

---
+++ schema/platoon_leaders.sql
@@ -0,0 +1,10 @@
+-- Run platoons.sql and cadet_officers.sql before
+-- running this script
+CREATE TABLE IF NOT EXISTS platoon_leaders (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  enrolled_cadet_id INT NOT NULL,
+  platoon_id INT NOT NULL,
+  appointed_on DATE NOT NULL,
+  FOREIGN KEY(enrolled_cadet_id) REFERENCES cadet_officers(enrolled_cadet_id),
+  FOREIGN KEY(platoon_id) REFERENCES platoons(id)
+);
