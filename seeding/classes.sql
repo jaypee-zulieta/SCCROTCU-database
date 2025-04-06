44ddda321ee3916df628590a8480845c102037b7
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-06 10:16:34 +0800

    feat: Seed ranks and classes table
    Seed this table so that we can have some initial
    data to work with.

---
+++ seeding/classes.sql
@@ -0,0 +1,5 @@
+-- Run ./schema/classes.sql before running this script
+INSERT INTO classes (abbreviated_name, descriptive_name) VALUES
+("3CL", "Third Class"),
+("2CL", "Second Class"),
+("1CL", "First Class")
---
+++ seeding/ranks.sql
@@ -0,0 +1,8 @@
+-- Run ./schema/ranks.sql before running this script
+INSERT INTO ranks (abbreviated_name, descriptive_name) VALUES 
+("CDT 2LT", "Cadet Second Lieutenant"),
+("CDT 1LT", "Cadet First Lieutenant"),
+("CDT CPT", "Cadet Captain"),
+("CDT MAJ", "Cadet Major"),
+("CDT LTC", "Cadet Lieutenant Colonel"),
+("CDT COL", "Cadet Colonel");
