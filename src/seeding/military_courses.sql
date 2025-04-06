a36620e071e984d3a1f42c95603cb78b0f4fe0e8
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-07 04:44:31 +0800

    feat: Seed military_courses table
    Seed military_courses table with some data:
    Military Science 1, Military Science 2,
    Military Science 31 and Military Science 32.

---
+++ src/seeding/military_courses.sql
@@ -0,0 +1,5 @@
+INSERT INTO military_courses (abbreviated_name, descriptive_name) VALUES
+("MS1", "Military Science 1"),
+("MS2", "Military Science 2"),
+("MS31", "Military Science 31"),
+("MS32", "Military Science 32");
