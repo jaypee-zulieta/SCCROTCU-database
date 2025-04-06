7688e9f64479c578121897ae833387610c1722c9
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-07 05:13:50 +0800

    feat: Seed platoon_specializations table

---
+++ src/seeding/platoon_specializations.sql
@@ -0,0 +1,13 @@
+-- Run ./schema/platoon_specializations.sql 
+-- before running this script.
+INSERT INTO platoon_specializations (specialization_name) VALUES
+("Ceremonial"),
+("M16"),
+("Theoretical"),
+("Knot Tying"),
+("Map Reading"),
+("Responders"),
+("Theoretical Medics"),
+("Office Personnel"),
+("Platoon Unit Tactics"),
+("Band");
