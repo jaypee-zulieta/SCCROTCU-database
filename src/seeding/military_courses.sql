301f3d1749684acd70b6ca0cbda33c2d701e1f06
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-07 05:11:59 +0800

    doc: Add helpful comment at the top of file
    Add a comment to remind the user to run
    /schema/military_courses.sql before running the
    /seeding/military_courses.sql script.

--- src/seeding/military_courses.sql
+++ src/seeding/military_courses.sql
@@ -1,3 +1,4 @@
+-- Run ./schema/military_courses.sql before running this script.
 INSERT INTO military_courses (abbreviated_name, descriptive_name) VALUES
 ("MS1", "Military Science 1"),
 ("MS2", "Military Science 2"),
