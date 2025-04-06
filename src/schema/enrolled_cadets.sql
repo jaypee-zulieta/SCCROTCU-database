ff9562efa773e008eaedf84322720299096e880b
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-07 04:37:32 +0800

    fix: Add join table for semesters and courses
    Add a join table for semesters and courses so that
    we can offer many courses for many semesters
    instead of having to insert more courses in the
    database.

--- src/schema/enrolled_cadets.sql
+++ src/schema/enrolled_cadets.sql
@@ -9,3 +9,28 @@ CREATE TABLE IF NOT EXISTS enrolled_cadets (
   CONSTRAINT military_course_foreign_key
   FOREIGN KEY (military_course_id) REFERENCES military_courses(id)
 );
+
+
+-- This code will create a join table for military_courses
+-- and semesters called offered_courses. This will allow
+-- the database to offer many courses per semester.
+ALTER TABLE enrolled_cadets DROP CONSTRAINT military_course_foreign_key;
+ALTER TABLE enrolled_cadets DROP COLUMN military_course_id;
+
+--@block
+ALTER TABLE military_courses DROP CONSTRAINT military_courses_ibfk_1;
+ALTER TABLE military_courses DROP COLUMN semester_id;
+
+CREATE TABLE offered_courses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  semester_id INT NOT NULL,
+  military_course_id INT NOT NULL,
+  CONSTRAINT military_course_fk 
+  FOREIGN KEY(military_course_id) REFERENCES military_courses(id),
+  FOREIGN KEY(semester_id) REFERENCES semesters(id)
+);
+
+--@block
+ALTER TABLE enrolled_cadets ADD COLUMN offered_course_id INT NOT NULL;
+ALTER TABLE enrolled_cadets ADD CONSTRAINT FOREIGN KEY(offered_course_id)
+REFERENCES offered_courses(id);
