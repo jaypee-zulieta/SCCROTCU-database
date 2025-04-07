ab5cbd50e798309898cd73bb8973004b85d94550
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-08 04:30:17 +0800

    fix: Remove fields from find_all_battalions query
    Remove the brigade field from the query because,
    if we follow standard REST practices it would be
    unnecessary.

--- src/queries/find_all_battalions.sql
+++ src/queries/find_all_battalions.sql
@@ -1,7 +1,5 @@
 SELECT 
-  b.id AS id, 
-  b.descriptive_name AS battalion_name,
-  br.descriptive_name AS brigade,
-  b.created_on AS created_on
-FROM battalions AS b LEFT JOIN brigades AS br 
-ON b.brigade_id = br.id;
+  id, 
+  descriptive_name,
+  created_on
+FROM battalions;
