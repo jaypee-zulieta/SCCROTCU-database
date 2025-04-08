b163f3af58fec886d6546bf8668f90d2d03092f7
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-09 05:25:25 +0800

    feat: Filter the results of the battalions query
    Filter the results of the find-all-battalions
    query to only show the battalions that are not
    part of any brigade.

--- src/queries/find_all_battalions.sql
+++ src/queries/find_all_battalions.sql
@@ -1,5 +1,9 @@
+-- Should be mapped to a route called
+-- /battalions
+-- Returns  all battalions that are not part of brigades
 SELECT 
   id, 
   descriptive_name,
   created_on
-FROM battalions;
+FROM battalions
+WHERE brigade_id = NULL;
