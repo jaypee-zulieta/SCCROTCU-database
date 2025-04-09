1669bb8bd7d447ac4a2cd59c7b72c2dff02f5750
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-10 06:33:26 +0800

    feat(query): Add a query to find all battalions
    Add a query file to find all the battalions in
    the unit.

---
+++ src/queries/find_battalions.sql
@@ -0,0 +1,11 @@
+-- Finds all the battalions in the unit
+-- Should be mapped to a route called '/battalions'
+-- battalions don't have to be part of a brigade.
+SELECT 
+  b.id, 
+  b.descriptive_name, 
+  b.brigade_id
+  b.created_on 
+FROM battalions as b
+ORDER BY b.created_on DESC
+LIMIT 15
