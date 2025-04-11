542337d6ff18d7755e6af5feb0d3835459bfd8c6
Jaypee P. Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-04-11 18:39:23 +0800

    feat: Add a query file to find a single battalion
    This query file will find a specific battalion
    that belongs to a specific brigade.

---
+++ src/queries/battalions/find_battalion_belonging_to_brigade.sql
@@ -0,0 +1,11 @@
+-- Finds a specific battalion belonging to a specific brigade
+-- Should be mapped to a route called '/brigades/{brigade-id}/battalions/{battalion-id}'
+SELECT 
+  b.id,
+  b.descriptive_name,
+  b.brigade_id,
+  b.created_on
+FROM battalions AS b
+WHERE b.brigade_id = 1000 -- id of the brigade
+AND b.id = 10000 -- id of the battalion
+LIMIT 50
